53163beac7fd33f0fb057698e17fbf918f0b640e
Wahab Noor <104102500+wahab3913@users.noreply.github.com>
2022-11-23 23:36:45 +0500

    Delete Server directory
    del

--- Server/Server.js
+++
@@ -1,9 +0,0 @@
-const http=require('http')
-const app=require('./app')
-
-const server=http.createServer(app)
-const port=process.env.PORT || 3000
-
-server.listen(port,()=>{
-    console.log('Server is running on port '+port)
-})
--- Server/api/displayproduct.js
+++
@@ -1,9 +0,0 @@
-const express=require('express');
-const router=express.Router();
-const displayproduct=require('../Displaypro.json');
-
-router.get('/',(req,res,next)=>{
-    res.status(200).json(displayproduct);
-    console.log(displayproduct);
-})
-module.exports=router;
