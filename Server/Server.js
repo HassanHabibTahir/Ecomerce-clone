ba519e067403c8d01456554b68b5aa00bcf69557
Wahab Noor <104102500+wahab3913@users.noreply.github.com>
2022-11-23 18:23:57 +0500

    server

---
+++ Server/Server.js
@@ -0,0 +1,9 @@
+const http=require('http')
+const app=require('./app')
+
+const server=http.createServer(app)
+const port=process.env.PORT || 3000
+
+server.listen(port,()=>{
+    console.log('Server is running on port '+port)
+})
